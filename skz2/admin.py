7c0a16fdd860bbd80dd5ca248dc1a79c398039a6
atasatamatara <atasatamatara@gmail.com>
2012-03-03 23:00:05 +0900

    init

---
+++ skz2/admin.py
@@ -0,0 +1,13 @@
+from yonda.models import *
+from django.contrib import admin
+
+#class UserAdmin(admin.ModelAdmin):
+    #list_display = ('name', 'ctime',)
+    #readonly_fields = ('ctime',)
+
+#class UrlAdmin(admin.ModelAdmin):
+    #list_display = ('url', 'title', 'user', 'ctime','atime',)
+    #readonly_fields = ('ctime', 'atime')
+
+#admin.site.register(User, UserAdmin)
+#admin.site.register(Url, UrlAdmin)
