a8aa8cefebf413ccedcd6c0543dbaca2626dd208
atasatamatara <atasatamatara@gmail.com>
2012-03-08 00:27:39 +0900

    オブジェクトの保存がわっかんねーわ

--- skz2/models.py
+++ skz2/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import pickle
 
 # Create your models here.
+#class Tweet(models.Model):
+class Tweet(object):
+    #def __init__(self, status):
+        #self.status = status
+    #status = models.CharField(u"Status", max_length=256)
+    #self.status = status
+
+    #def __unicode__(self):
+        #return self.status
+
+    #class Meta:
+        #db_table = 'Status'
