3ecb79d641b24d0cd4c3f7cbee4bcfb934f3cd1d
atasatamatara <atasatamatara@gmail.com>
2012-03-08 21:25:48 +0900

    仮そめのpush

--- skz2/models.py
+++ skz2/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-import pickle
+#-*- coding: utf-8 -*-
 
-# Create your models here.
-#class Tweet(models.Model):
-class Tweet(object):
-    #def __init__(self, status):
-        #self.status = status
-    #status = models.CharField(u"Status", max_length=256)
-    #self.status = status
+class Tweet(models.Model):
+    user_id = models.CharField(u"ユーザー固有のID", max_length=10)
+    name = models.CharField(u"name", max_length=30)
+    screen_name = models.CharField(u"screen_name", max_length=20)
+    text = models.CharField(u"本文", max_length=140)
+    in_reply_to = models.CharField(u"in_reply_to", max_length=30)
+    since_id = models.CharField(u"since_id", max_length=30)
+    favorited = models.BooleanField(u"fav", default=false)
+    created_at = models.CharField(u'作成日時', max_length=40)
 
-    #def __unicode__(self):
-        #return self.status
+    def __unicode__(self):
+        return "%s:%s" % (self.screen_name, self.text)
 
-    #class Meta:
-        #db_table = 'Status'
+    class Meta:
+        db_table = 'Tweet'
--- skz2/views.py
+++ skz2/views.py
@@ -1,1 +1,29 @@
 # Create your views here.
+#-*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+#from django.http import HttpResponse
+from django.views.generic.simple import direct_to_template
+#from django.contrib.csrf.middleware import csrf_exempt
+#from django.shortcuts import get_object_or_404
+#from django.core.mail import send_mail
+import tweepy
+from see import see
+
+#from yonda.forms import *
+#from yonda.models import *
+#from yonda.tools import use_username_or_masuda
+
+def index(request):
+    """トップページ"""
+    #loginしてるとき
+    if request.method == "GET":
+        return direct_to_template(request, "index.html", {"form":UrlPostForm()})
+    if request.method == "POST":
+        form = UrlPostForm(request.POST)
+        if not form.is_valid():
+            return HttpResponseRedirect(reverse('index'))
+        user = use_username_or_masuda(request)
+        Url.post_url(form.cleaned_data["url"], user)
+        return HttpResponseRedirect(reverse('index'))
+
