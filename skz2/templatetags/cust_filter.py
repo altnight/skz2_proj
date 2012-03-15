a24ec67b2b393a42237cc3ae27738ebd13b32949
atasatamatara <atasatamatara@gmail.com>
2012-03-15 17:42:10 +0900

    結局decode('utf-8')の問題かよ！！！！

--- skz2/templatetags/cust_filter.py
+++ skz2/templatetags/cust_filter.py
@@ -9,7 +9,7 @@ register = template.Library()
 
 @register.filter
 def user_mention(value, autoescape=None):
-    user = re.match(r'@([\w]+)', value)
+    user = re.search(r'@([\w]+)', value)
     if user is None:
         pass
     else:
@@ -19,10 +19,11 @@ user_mention.needs_autoescape = True
 
 @register.filter
 def hashtag(value, autoescape=None):
-    hashtag = re.match(r'#([\w一-龠ぁ-んァ-ヴー]+)', value)
+    hashtag = re.search(r'#([\w一-龠ぁ-んァ-ヴー]+)'.decode('utf-8'), value)
     if hashtag is None:
         pass
     else:
-        value= re.sub(r'#[\w一-龠ぁ-んァ-ヴー]+', '<a href=https://twitter.com/#!/search/%23' + hashtag.group(1) +'>#'+ hashtag.group(1)+'</a>', value)
+        print hashtag
+        value= re.sub(r'#([\w一-龠ぁ-んァ-ヴー]+)'.decode('utf-8'), '<a href=https://twitter.com/#!/search/%23' + hashtag.group(1) +'>#'+ hashtag.group(1)+'</a>', value)
     return mark_safe(value)
 hashtag.needs_autoescape = True
