eb5395ffabc2dd091c6fde5d1698388c27f19b67
atasatamatara <atasatamatara@gmail.com>
2012-03-15 16:35:32 +0900

    ちょい

--- skz2/templatetags/cust_filter.py
+++ skz2/templatetags/cust_filter.py
@@ -1,20 +1,30 @@
-#-*- encofing:utf-8 -*-
+#-*- coding: utf-8 -*-
 #from django.template.defaultfilters import stringfilter
 from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 import re
 register = template.Library()
 
 @register.filter
-#@stringfilter
-def user_mention(value):
-    user = re.match(r'(@[\w]+)', value)
+def user_mention(value, autoescape=None):
+    user = re.match(r'@([\w]+)', value)
     if user is None:
         pass
     else:
-        value= re.sub(r'@[\w]+', '<a href="https://twitter.com/#!/">user.group(1)</a>', value)
-    #if re.match(r'@[\w]+', value):
-        #re.sub(, , value)
-    return value
-#user_mention.is_safe=False
-#register.filter(user_mention)
+        value= re.sub(r'@[\w]+', '<a href=https://twitter.com/#!/' + user.group(1) +'>@'+ user.group(1)+'</a>', value)
+    return mark_safe(value)
+user_mention.needs_autoescape = True
+
+register = template.Library()
+
+@register.filter
+def hashtag(value, autoescape=None):
+    hashtag = re.match(r'#([\w一-龠ぁ-んァ-ヴー]+)', value)
+    if hashtag is None:
+        pass
+    else:
+        value= re.sub(r'#[\w一-龠ぁ-んァ-ヴー]+', '<a href=https://twitter.com/#!/search/' + hashtag.group(1) +'>#'+ hashtag.group(1)+'</a>', value)
+    return mark_safe(value)
+hashtag.needs_autoescape = True
