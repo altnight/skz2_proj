6bdc9e19562648dc57fb50eb71768e7339259486
atasatamatara <atasatamatara@gmail.com>
2012-03-16 17:28:05 +0900

    ur'正規表現'の方がスマートですね

--- skz2/templatetags/cust_filter.py
+++ skz2/templatetags/cust_filter.py
@@ -19,10 +19,10 @@ user_mention.needs_autoescape = True
 
 @register.filter
 def hashtag(value, autoescape=None):
-    hashtag = re.search(r'#([\w一-龠ぁ-んァ-ヴー]+)'.decode('utf-8'), value)
+    hashtag = re.search(ur'#([\w一-龠ぁ-んァ-ヴー]+)', value)
     if hashtag is None:
         pass
     else:
-        value= re.sub(r'#([\w一-龠ぁ-んァ-ヴー]+)'.decode('utf-8'), '<a href=https://twitter.com/#!/search/%23' + hashtag.group(1) +'>#'+ hashtag.group(1)+'</a>', value)
+        value= re.sub(ur'#([\w一-龠ぁ-んァ-ヴー]+)', '<a href=https://twitter.com/#!/search/%23' + hashtag.group(1) +'>#'+ hashtag.group(1)+'</a>', value)
     return mark_safe(value)
 hashtag.needs_autoescape = True
