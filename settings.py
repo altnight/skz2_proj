2cab830ee35197d0620bf5922ee1bc41628e1f8b
atasatamatara <atasatamatara@gmail.com>
2012-03-19 10:37:40 +0900

    開発時の静的ファイルの取り扱いについて

--- settings.py
+++ settings.py
@@ -61,6 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = ''
 
 # URL prefix for static files.
@@ -74,6 +75,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -108,7 +110,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'skz2_proj.urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,'templates')
+    os.path.join(BASE_DIR,'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -157,6 +159,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     #'yonda.context_processors.domain',
     'skz2.context_processors.display_user',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
