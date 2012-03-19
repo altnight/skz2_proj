08a10059d545afc6f3814624e02f95eb00e3c5be
atasatamatara <atasatamatara@gmail.com>
2012-03-19 10:42:47 +0900

    if debugを追加

--- urls.py
+++ urls.py
@@ -17,4 +17,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.STATIC_ROOT}),
+if settings.DEBUG:
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root' : settings.STATIC_ROOT}),
