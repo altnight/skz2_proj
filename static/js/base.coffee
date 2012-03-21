839c2df44b35e46f136db1620a95ab75ab8dcfce
atasatamatara <atasatamatara@gmail.com>
2012-03-21 13:13:31 +0900

    文字色カエルとか

--- static/js/base.coffee
+++ static/js/base.coffee
@@ -1,25 +1,42 @@
 $(document).ready ->
     $('#status').on('focus', ->
-        @.style.rows = 8
-        @.style.cols = 80
-        @.style.width = "400px"
-        @.style.height = "80px"
+        $(@).css('rows', 8)
+        $(@).css('cols', 80)
+        $(@).css('width', '400px')
+        $(@).css('height', '80px')
     )
     $('#status').on('blur', ->
-        @.style.rows = 1
-        @.style.cols = 30
-        @.style.width = "227px"
-        @.style.height = "13px"
+        $(@).css('rows', 1)
+        $(@).css('cols', 30)
+        $(@).css('width', '227px')
+        $(@).css('height', '13px')
     )
+
+    max_length= 140
+    $('#count').text(max_length)
+
     $('#status').on('keyup', (ev)->
         if ev.keyCode == 13 #Enter
-            alert '発言しました'
+            if max_length < $(@).val().length
+                alert '140字を超えています'
+                return false
             $.ajax
                 type: "GET"
                 data:
-                    q: $('#status').val()
+                    q: $(@).val()
                 url: "http://192.168.56.101:8000/update_status"
                 dataTpye: "json"
-            テキストエリアを消す
-            $('#status').val('')
+            alert '発言しました'
+            #テキストエリアを消す
+            $(@).val('')
+    )
+
+
+    $('#status').on('keyup', (ev)->
+        $('#count').text(max_length - $(@).val().length)
+        if max_length < $(@).val().length
+            $('#count').css('color', 'red')
+        else
+            $('#count').css('color', 'black')
+
     )
--- static/js/base.js
+++ static/js/base.js
@@ -1,30 +1,44 @@
 
 $(document).ready(function() {
+  var max_length;
   $('#status').on('focus', function() {
-    this.style.rows = 8;
-    this.style.cols = 80;
-    this.style.width = "400px";
-    return this.style.height = "80px";
+    $(this).css('rows', 8);
+    $(this).css('cols', 80);
+    $(this).css('width', '400px');
+    return $(this).css('height', '80px');
   });
   $('#status').on('blur', function() {
-    this.style.rows = 1;
-    this.style.cols = 30;
-    this.style.width = "227px";
-    return this.style.height = "13px";
+    $(this).css('rows', 1);
+    $(this).css('cols', 30);
+    $(this).css('width', '227px');
+    return $(this).css('height', '13px');
   });
-  return $('#status').on('keyup', function(ev) {
+  max_length = 140;
+  $('#count').text(max_length);
+  $('#status').on('keyup', function(ev) {
     if (ev.keyCode === 13) {
-      alert('発言しました');
+      if (max_length < $(this).val().length) {
+        alert('140字を超えています');
+        return false;
+      }
       $.ajax({
         type: "GET",
         data: {
-          q: $('#status').val()
+          q: $(this).val()
         },
         url: "http://192.168.56.101:8000/update_status",
         dataTpye: "json"
       });
-      テキストエリアを消す;
-      return $('#status').val('');
+      alert('発言しました');
+      return $(this).val('');
+    }
+  });
+  return $('#status').on('keyup', function(ev) {
+    $('#count').text(max_length - $(this).val().length);
+    if (max_length < $(this).val().length) {
+      return $('#count').css('color', 'red');
+    } else {
+      return $('#count').css('color', 'black');
     }
   });
 });
