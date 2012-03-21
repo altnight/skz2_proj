00938f55d903f5a340a29b3f45119120d53b40c0
atasatamatara <atasatamatara@gmail.com>
2012-03-21 16:16:17 +0900

    不要なmapperを削除

--- skz2/mappers.py
+++ skz2/mappers.py
@@ -11,14 +11,3 @@ class ListsMapper(ModelMapper):
     class Meta:
         model = Lists
         exclude = 'user', 'ctime', 'atime'
-
-class RateLimit(object):
-    def __init__(self, remaining, hourly_limit, reset_time):
-        self.remaining = remaining
-        self.hourly_limit = hourly_limit
-        self.reset_time = reset_time
-
-class RateLimitMapper(Mapper):
-    remaining = RawField()
-    hourly_limit = RawField()
-    reset_time = RawField()
